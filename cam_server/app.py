364ede49d374479a16fe577df7bed5fc67798717
Jose Luis <joseluisrojasaranda@gmail.com>
2019-12-07 06:18:24 -0600

    Whatsapp bot

--- cam_server/app.py
+++ cam_server/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 from flask import request
 from flask import abort
+import threading
 import requests
 import json
 import cv2
 import base64
 import numpy as np
 
+
 from flask_cors import CORS
 from datetime import datetime
+import subprocess
+
 
 
 app = Flask(__name__)
@@ -18,10 +22,13 @@ CORS(app)
 coco_ip = "http://localhost:"
 coco_port = "3000"
 
+wa_ip = "http://localhost:"
+wa_port = "3006"
+
 # HARDCORE DATABASE
 cache = {
     "active": True,
-    "fb_id": None}
+    "wa": False}
 
 @app.after_request
 def after_request(response):
@@ -49,6 +56,22 @@ def get_livefeed():
         "people": cache["people"],
         "datetime": cache["datetime"]})
 
+@app.route("/vigia/api/get_act", methods=["GET"])
+def get_activatiob():
+    return jsonify({
+        "activate": cache["active"]})
+
+@app.route("/vigia/api/set_act", methods=["POST"])
+def set_activation(): 
+    data = request.json
+    cache["activate"] = data.activate;
+
+    return "jalo"
+
+def wa_notification():
+    r = requests.get(wa_ip+wa_port)
+
+
 def render_boxes(image, predictions):
     for pred in predictions:
         if pred["score"] >= 0.5:
@@ -66,6 +89,10 @@ def draw_predictions(image, predictions):
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     render_boxes(image, predictions)
+
+    cv2.imwrite("prueba.jpg", image)
+
+    subprocess.run(["osascript", "-e", 'set the clipboard to (read (POSIX file "/Users/jlreds/Hackathon-SLP-2019/cam_server/prueba.jpg") as JPEG picture)'])
 
     _, buffer = cv2.imencode(".jpg", image)
     b64_img = base64.b64encode(buffer)
@@ -88,12 +115,16 @@ def recieve_image():
         r_coco = requests.post(coco_ip+coco_port, data={"img": cam_data["img"]})
         coco_predictions = json.loads(r_coco.text)
 
+        people = len(coco_predictions)
         cache["people"] = len(coco_predictions)
         cache["last_frame"] = draw_predictions(cam_data["img"], coco_predictions.copy())
+        if people > 0 and cache["wa"] == False:
+            cache["wa"] = True
+            t = threading.Thread(target=wa_notification)
     else:
         cache["people"] = 0
         cache["last_frame"] = cam_data["img"]
-
+        cache["wa"] = False
 
     # date time
     c_time = datetime.now()
---
+++ fb_bot/botnet.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import sys
+import subprocess
+from flask import Flask, jsonify
+from flask import request
+
+app = Flask(__name__)
+
+# Replace below path with the absolute path
+# to chromedriver in your computer
+driver = webdriver.Chrome('./chromedriver')
+
+
+# Replace 'Friend's Name' with the name of your friend 
+# or the name of a group 
+@app.route("/")
+def test():
+
+    target = '"Quadruple Thread"'
+
+    # Replace the below string with your own message
+    string = "Te estan robando perro."
+
+    x_arg = '//span[contains(@title,' + target + ')]'
+    group_title = wait.until(EC.presence_of_element_located((
+        By.XPATH, x_arg)))
+    group_title.click()
+
+    time.sleep(1)
+
+    message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+    subprocess.run(["osascript", "-e", 'tell application "System Events" to keystroke "v" using command down'])
+
+    time.sleep(3)
+    subprocess.run(["osascript", "-e", 'tell application "System Events" to key code 36'])
+    #subprocess.run(["osascript", "-e", 'tell application "System Events" to keystroke "enter" using command down'])
+
+    #sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
+    #sendbutton.click()
+
+
+    #driver.close()
+
+    return "ahuevo"
+
+if __name__ == "__main__":
+    driver.get("https://web.whatsapp.com/")
+    wait = WebDriverWait(driver, 600)
+    time.sleep(15)
+    app.run(host="0.0.0.0", port=3006, debug=True)
+    print("SERVER RUNNING")
+
